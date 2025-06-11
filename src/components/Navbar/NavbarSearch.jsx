3868573f40da377a753b671acba0c78375ae74ec
root <7hlsn1@gmail.com>
2025-06-11 17:26:44 -0300

    update

--- src/components/Navbar/NavbarSearch.jsx
+++ src/components/Navbar/NavbarSearch.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-
+import styles from './NavbarSearch.module.scss'
 export const NavbarSearch = () => (
-    <form className="search-bar d-flex flex-grow-1 mx-3" style={{ maxWidth: '600px' }}>
-      <input
-        className="form-control rounded-start"
-        type="search"
-        placeholder="Buscar no site..."
-        aria-label="Search"
-      />
-      <button className="btn btn-white rounded-end px-3" type="submit">
-        <i className="bi bi-search text-white"></i>
-      </button>
-    </form>
-  );
+  <form className={`search-bar d-flex flex-grow-1 mx-3 ${styles.form}`} style={{ maxWidth: '600px' }}>
+    <input
+      className="form-control rounded-start"
+      type="search"
+      placeholder="Buscar no site..."
+      aria-label="Search"
+    />
+    <button className="btn btn-white rounded-end px-3" type="submit">
+      <i className="bi bi-search text-white"></i>
+    </button>
+  </form>
+);
